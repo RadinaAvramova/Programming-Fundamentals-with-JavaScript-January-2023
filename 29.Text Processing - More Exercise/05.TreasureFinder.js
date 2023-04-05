749e8b0d21d6a821f96a2f6b8f2e57c08a772faa
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-04-05 10:51:54 +0300

    Add files via upload

---
+++ 29.Text Processing - More Exercise/05.TreasureFinder.js
@@ -0,0 +1,14 @@
+
+
+
+treasureFinder(["1 2 1 3",
+
+"ikegfp'jpne)bv=41P83X@",
+
+"ujfufKt)Tkmyft'duEprsfjqbvfv=53V55XA","find"]);
+
+treasureFinder(["1 4 2 5 3 2 1",
+
+`Ulgwh"jt$ozfj!'kqqg(!bx"A3U237GC`,
+
+"tsojPqsf$(lrne'$CYfqpshksdvfT$>634O57YC","'stj)>34W68Z@","find"])
