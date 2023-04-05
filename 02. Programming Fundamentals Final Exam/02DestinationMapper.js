c7c33339ee3dd881416d1b8875debbb3077cd341
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-04-05 11:26:26 +0300

    Add files via upload

---
+++ 02. Programming Fundamentals Final Exam/02DestinationMapper.js
@@ -0,0 +1,18 @@
+function destinationMapper(destination) {
+    string pattern = @"(=|\/)(?<location>[A-Z][A-z]{2,})\1";
+    let count = 0;
+    let ourCity = [];
+ 
+    if (pattern.test(destination)) {
+        const ourDest = destination.match(pattern);
+ 
+        ourDest.forEach(destination => {
+            count += (destination.length - 2);
+            ourCity.push(destination.slice(1, (destination.length - 1)));
+ 
+        });
+    }
+    
+        console.log(`Destinations: ${ourCity.join(", ")}`);
+        console.log(`Travel Points: ${count}`);  
+}
